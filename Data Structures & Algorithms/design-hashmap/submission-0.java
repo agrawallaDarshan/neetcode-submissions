c3b57359503a7fa91f5256d9802ca16858013646
Darshan Agrawalla <81975565+agrawallaDarshan@users.noreply.github.com>
2026-07-02 01:20:47 +0530

    Add: design-hashmap - submission-0

---
+++ Data Structures & Algorithms/design-hashmap/submission-0.java
@@ -0,0 +1,81 @@
+class Node {
+    int key;
+    int value;
+    Node prev;
+    Node next;
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+class MyHashMap {
+    int BUCKET_LIMIT = 100;
+    Node[] arr;
+
+    public MyHashMap() {
+        arr = new Node[BUCKET_LIMIT];
+    }
+
+    public void put(int key, int value) {
+        int hashedValue = key % BUCKET_LIMIT;
+        if (arr[hashedValue] == null) {
+            Node node = new Node(key, value);
+            arr[hashedValue] = node;
+        } else {
+            Node start = arr[hashedValue], prev = null;
+            while (start != null && start.key != key) {
+                prev = start;
+                start = start.next;
+            }
+            if (start == null) {
+                // append the key to the end of the DLL
+                Node node = new Node(key, value);
+                node.prev = prev;
+                prev.next = node;
+            } else {
+                start.value = value;
+            }
+        }
+    }
+
+    public int get(int key) {
+        int hashedValue = key % BUCKET_LIMIT;
+        Node start = arr[hashedValue];
+        while (start != null && start.key != key) {
+            start = start.next;
+        }
+        if (start == null)
+            return -1;
+        return start.value;
+    }
+
+    public void remove(int key) {
+        int hashedValue = key % BUCKET_LIMIT;
+        Node start = arr[hashedValue];
+        // if it's the head
+        if (start != null && start.key == key) {
+            arr[hashedValue] = start.next;
+            start.next = null;
+            return;
+        }
+        while (start != null && start.key != key) {
+            start = start.next;
+        }
+        if (start == null)
+            return;
+        start.prev.next = start.next;
+        start.next.prev = start.prev;
+        start.next = null;
+        start.prev = null;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
